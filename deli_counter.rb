06b9a0db07cc625e4ea7e3783c1d87abf34a0c85
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-08 19:32:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+
+    array.each do |name|
+      message << " #{array.index(name) + 1}. #{name}"
+    end
+    puts message
+  end
+end
+
+def take_a_number(position, name)
+  position << name
+  puts "Welcome, #{name}. You are number #{position.index(name) + 1} in line."
+end
+
+def now_serving(position, name)
+
+  puts "Currently serving #{ position.index(name) - 1} in line."
+
+  puts "There is nobody waiting to be served!"
+ end

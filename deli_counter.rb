4eca99ef0d2a7883f2c96f44021bfb8f0d19022e
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-08 19:42:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,11 @@ def take_a_number(position, name)
   puts "Welcome, #{name}. You are number #{position.index(name) + 1} in line."
 end
 
-def now_serving(position, name)
-  position << name
-
-  puts "Currently serving #{position.index(name) - 1} in line."
-
-  puts "There is nobody waiting to be served!"
- end
+def now_serving(position)
+  if position == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{position.index(name)}."
+    end
+  end
+end
